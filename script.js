6f621a4b964d36820c875dd93540e8d315ca268a
12 <21>
2019-04-08 16:03:12 +0300

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+// $('.carousel.carousel-slider').carousel({
+//   fullWidth: true
+// });
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+$(document).ready(function () {
+  $('.parallax').parallax();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
