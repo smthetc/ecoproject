b4f06a6af1ee4f8787d85110a1c069914d600d90
ZaBralow <pavlik.elsukov@mail.ru>
2019-04-11 10:20:20 +0300

    fix

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ $(document).ready(function () {
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
+$(document).ready(function() {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+$( document ).ready(function() {
+  $("#button").click(
+  function(){
+    sendAjaxForm('results', 'ecorep', 'action.php');
+    return false; 
+  }
+);
+});
+
+function sendAjaxForm(results, ecorep, url) {
+  $.ajax({
+      url:     url, //url страницы
+      type:     "POST", //метод отправки
+      dataType: "html", //формат данных
+      data: $("#"+ecorep).serialize()
+ });
+}
